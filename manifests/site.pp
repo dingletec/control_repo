1e23eedf6658f2b28d79b0bacb51c63350bc3e58
Baird Carr <dingletec@gmail.com>
2022-04-10 16:24:29 -0500

    ensure terminator installed

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,7 +4,7 @@ node default {
     content => 'This is a readme',
     owner   => 'root',
   }
-  file {'/root/README':
-    owner => 'root',
+  package {'terminator':
+    ensure => installed,
   }
 }
